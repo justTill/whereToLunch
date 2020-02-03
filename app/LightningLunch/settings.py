dbe1089e84de1485dcc05d74aab13156c9569da9
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-02-03 17:20:10 +0100

    another test

--- app/LightningLunch/settings.py
+++ app/LightningLunch/settings.py
@@ -151,7 +151,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=app',
+    '--cover-package=.',
 ]
 
 RESET_VOTES_MIN = 30
