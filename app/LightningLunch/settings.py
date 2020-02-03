676e16e63beb57b3c72dcb77c88e46f4988b2865
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-02-03 17:07:23 +0100

    test

--- app/LightningLunch/settings.py
+++ app/LightningLunch/settings.py
@@ -25,7 +25,6 @@ DEBUG = bool(os.environ.get("DEBUG", default=False))
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default='localhost 127.0.0.1 [::1]').split(" ")
 
 # Application definition
-
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'weather.apps.WeatherConfig',
