73a2d7f69e811d9df68ceb86e15ffbe6be4df66f
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-02-10 09:19:03 +0100

    some clean up

--- app/LightningLunch/test_settings.py
+++ app/LightningLunch/test_settings.py
@@ -5,5 +5,3 @@ DJANGO_ALLOWED_HOSTS = ['*']
 SECRET_KEY = 'I AM SECRET'
 RESET_VOTES_MIN = int((datetime.datetime.now() + datetime.timedelta(minutes=1)).minute)
 RESET_VOTES_HOUR = int(datetime.datetime.now().hour)
-
-
