bbdd8b5c15e52c508bbfe58f559741755cf67190
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-02-13 09:34:15 +0100

    add Logging for debug file deletion

--- app/utils/clearLogs.py
+++ app/utils/clearLogs.py
@@ -1,15 +1,17 @@
 import os
+import structlog
 
 MAX_FILE_SIZE_DEBUG = 500000
 MAX_FILE_SIZE_AUDIT = 200000
+logger = structlog.getLogger('audit')
 
 
 def clear_debug_logs():
     if os.path.exists("logs/debug.log"):
-        if os.path.getsize("logs/debug.log") >= MAX_FILE_SIZE:
+        if os.path.getsize("logs/debug.log") >= MAX_FILE_SIZE_DEBUG:
             os.remove("logs/debug.log")
     else:
-        print("The debug file does not exist")
+        logger.critical("Debug log file does not exist")
 
 
 def clear_audit_logs():
@@ -17,4 +19,4 @@ def clear_audit_logs():
         if os.path.getsize("logs/audit.log") >= MAX_FILE_SIZE_AUDIT:
             os.remove("logs/audit.log")
     else:
-        print("The audit file does not exist")
+        logger.critical("Audit log file does not exist")
