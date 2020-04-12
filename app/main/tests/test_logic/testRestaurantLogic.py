4380e3ff22110f6c1e6a1af51f547650372ce4ae
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-04-12 15:21:35 +0200

    removed dependencies to testSetUp

--- app/main/tests/test_logic/testRestaurantLogic.py
+++ app/main/tests/test_logic/testRestaurantLogic.py
@@ -1,9 +1,25 @@
-from main.tests.testSetUpP import SetUpTestss
+from django.contrib.auth.models import User
+from django.test import TestCase
 from main.controller.logic import RestaurantLogic
-from main.model.models import Restaurant
+from main.model.models import Restaurant, Vote
 
 
-class RestaurantLogicTest(SetUpTestss):
+class RestaurantLogicTest(TestCase):
+
+    def setUp(self):
+        first_restaurant = Restaurant.objects.create(restaurant_name='first_restaurant')
+        second_restaurant = Restaurant.objects.create(restaurant_name='second_restaurant')
+        third_restaurant = Restaurant.objects.create(restaurant_name='third_restaurant')
+        Restaurant.objects.create(restaurant_name='fourth_restaurant')
+
+        first_test_user = User.objects.create(username='first_test_user', is_staff=True)
+        first_test_user.set_password('12345')
+        first_test_user.save()
+
+        Vote.objects.create(restaurant=first_restaurant, user=first_test_user)
+        Vote.objects.create(restaurant=first_restaurant, user=first_test_user)
+        Vote.objects.create(restaurant=third_restaurant, user=first_test_user)
+        Vote.objects.create(restaurant=second_restaurant, user=first_test_user)
 
     def test_restaurantsWithVotes(self):
         restaurantLogic = RestaurantLogic()
@@ -25,4 +41,5 @@ class RestaurantLogicTest(SetUpTestss):
 
         # Check if they are Sorted after Votes
         # second Criteria are Names
-        self.assertEquals(restaurantSet.items(), {(first_restaurant, 2), (third_restaurant, 1), (second_restaurant, 1), (fourth_restaurant, 0)})
+        self.assertEquals(restaurantSet.items(),
+                          {(first_restaurant, 2), (third_restaurant, 1), (second_restaurant, 1), (fourth_restaurant, 0)})
