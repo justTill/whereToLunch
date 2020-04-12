e2cba6183b6d1a842de3fbaccb5da25bb1145605
Till Pilarczyk <till.pilarczyk@rewe-digital.com>
2020-04-12 15:02:42 +0200

    wip

--- app/main/tests/test_logic/testVoteLogic.py
+++ app/main/tests/test_logic/testVoteLogic.py
@@ -1,15 +1,27 @@
 from django.contrib.auth.models import User
+from django.test import TestCase
 from main.controller.logic import VoteLogic
-from main.tests.testSetUpP import SetUpTestss
 from main.model.models import Restaurant, Vote
 
 
-class VoteLogicTest(SetUpTestss):
+class VoteLogicTest(TestCase):
     voteLogic = VoteLogic()
+
+    def setUp(self):
+        first_restaurant = Restaurant.objects.create(restaurant_name='first_restaurant')
+        second_restaurant = Restaurant.objects.create(restaurant_name='second_restaurant')
+
+        first_test_user = User.objects.create(username='first_test_user', is_staff=True)
+        first_test_user.set_password('12345')
+        first_test_user.save()
+        Vote.objects.create(restaurant=first_restaurant, user=first_test_user)
+
+        second_test_user = User.objects.create(username='second_test_user', is_staff=True)
+        second_test_user.set_password('12345')
+        second_test_user.save()
 
     def test_choice_of_the_day(self):
         choice_of_the_day = self.voteLogic.choice_of_the_day()
-
         self.assertEquals(len(choice_of_the_day), 1)
         self.assertIn('first_restaurant', choice_of_the_day[0].__str__())
 
@@ -17,13 +29,8 @@ class VoteLogicTest(SetUpTestss):
         user.set_password('12345')
         user.save()
 
-        user2 = User.objects.create(username='test_user2', is_staff=True)
-        user2.set_password('12345')
-        user2.save()
-
         restaurant = Restaurant.objects.create(restaurant_name='restaurant')
         Vote.objects.create(restaurant=restaurant, user=user)
-        Vote.objects.create(restaurant=restaurant, user=user2)
 
         choice_of_the_day = self.voteLogic.choice_of_the_day()
         self.assertIn('first_restaurant', choice_of_the_day[0].__str__())
